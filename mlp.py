97ec3cade9f484e86ed81efe0669dc9dc4fa403b
Wenbin Zhang - T3600 <wilsonzhang10@gmail.com>
2019-06-26 14:18:39 -0400

    update mlp.py

--- mlp.py
+++ mlp.py
@@ -3,7 +3,7 @@ import pathlib
 import random
 
 
-tf.enable_eager_execution()
+# tf.enable_eager_execution()
 AUTOTUNE = tf.data.experimental.AUTOTUNE
 
 # data_root = '/home/wenbin/Workspace/tk-script/images'
@@ -28,12 +28,10 @@ for directory in dicts.keys():
     xys.append(dicts[directory])
     directs.append(directory)
 all_image_paths = directs
-all_image_labels = [xy[0] for xy in xys]
-all_image_x = [xy[0] for xy in xys]
-all_image_y = [xy[1] for xy in xys]
+all_image_x = [xy[0] / 1280 for xy in xys]
+all_image_y = [xy[1] / 1024 for xy in xys]
 image_count = len(all_image_paths)
 print(len(all_image_paths))
-print(len(all_image_labels))
 
 img_path = all_image_paths[0]
 print(img_path)
@@ -44,11 +42,11 @@ img_tensor = tf.image.decode_image(img_raw)
 print(img_tensor.shape)
 print(img_tensor.dtype)
 
-img_final = tf.image.resize_images(img_tensor, [192, 192])
-img_final = img_final/255.0
-print(img_final.shape)
-print(img_final.numpy().min())
-print(img_final.numpy().max())
+# img_final = tf.image.resize_images(img_tensor, [192, 192])
+# img_final = img_final/255.0
+# print(img_final.shape)
+# print(img_final.numpy().min())
+# print(img_final.numpy().max())
 
 def preprocess_image(image):
   image = tf.image.decode_jpeg(image, channels=3)
@@ -66,27 +64,15 @@ print(path_ds)
 
 image_ds = path_ds.map(load_and_preprocess_image, num_parallel_calls=AUTOTUNE)
 
-# import matplotlib.pyplot as plt
+label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(all_image_x, tf.float32))
 
-# plt.figure(figsize=(8,8))
-# for n,image in enumerate(image_ds.take(4)):
-  # plt.subplot(2,2,n+1)
-  # plt.imshow(image)
-  # plt.grid(False)
-  # plt.xticks([])
-  # plt.yticks([])
+# for label in label_ds.take(3):
+    # print(label)
 
-# plt.show()
+ds = tf.data.Dataset.from_tensor_slices((all_image_paths, all_image_x))
 
-label_ds = tf.data.Dataset.from_tensor_slices(tf.cast(all_image_labels, tf.int64))
-
-for label in label_ds.take(3):
-    print(label)
-
-ds = tf.data.Dataset.from_tensor_slices((all_image_paths, all_image_x, all_image_y))
-
-def load_and_preprocess_from_path_label(path, x, y):
-    return load_and_preprocess_image(path), x, y
+def load_and_preprocess_from_path_label(path, x):
+    return load_and_preprocess_image(path), x
 
 image_label_ds = ds.map(load_and_preprocess_from_path_label)
 print(image_label_ds)
@@ -97,3 +83,26 @@ ds = ds.repeat()
 ds = ds.batch(BATCH_SIZE)
 ds = ds.prefetch(buffer_size=AUTOTUNE)
 print(ds)
+
+# def change_range(image, x, y):
+    # return 2*image-1, x, y
+# keras_ds = ds.map(change_range)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Flatten(input_shape=(192,192,1)),
+    tf.keras.layers.Dense(200, activation=tf.nn.leaky_relu),
+    tf.keras.layers.Dense(200, activation=tf.nn.leaky_relu),
+    tf.keras.layers.Dense(1)
+    ])
+# y_pred = model(ds)
+optimizer = tf.keras.optimizers.RMSprop(0.001)
+model.compile(loss=tf.losses.mean_squared_error,
+                optimizer=optimizer,
+                metrics=["accuracy"])
+init = tf.global_variables_initializer()
+with tf.Session() as sess:
+    sess.run(init)
+
+    for epoch in range(10):
+        model.fit(ds, steps_per_epoch=10)
+
